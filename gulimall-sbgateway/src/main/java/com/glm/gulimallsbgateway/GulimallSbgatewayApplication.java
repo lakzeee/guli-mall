b1fedd76acc3ee9dbe15b46fb9f0a1e0ad7d5a14
zehucai <caizh1997@live.com>
2022-11-20 16:51:34 +0800

    add spring cloud gateway component

---
+++ gulimall-sbgateway/src/main/java/com/glm/gulimallsbgateway/GulimallSbgatewayApplication.java
@@ -0,0 +1,17 @@
+package com.glm.gulimallsbgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+public class GulimallSbgatewayApplication {
+
+    public static void main(String[] args) {
+        System.setProperty("spring.cloud.bootstrap.enabled","true");
+        SpringApplication.run(GulimallSbgatewayApplication.class, args);
+    }
+
+}
---
+++ gulimall-sbgateway/src/test/java/com/glm/gulimallsbgateway/GulimallSbgatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.glm.gulimallsbgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallSbgatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
