a167078f758d3073b0359e08c1e722ecd74f106e
zehucai <caizh1997@live.com>
2022-11-21 23:20:15 +0800

    add test code for oss

--- gulimall-connect/src/main/java/com/glm/connect/GulimallConnectApplication.java
+++ gulimall-connect/src/main/java/com/glm/connect/GulimallConnectApplication.java
@@ -1,15 +1,42 @@
 package com.glm.connect;
 
+import com.aliyun.oss.OSS;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
 
 @EnableDiscoveryClient
 @SpringBootApplication
 public class GulimallConnectApplication {
-
+    public static final String BUCKET_NAME = "gulimall-lak";
     public static void main(String[] args) {
         System.setProperty("spring.cloud.bootstrap.enabled","true");
         SpringApplication.run(GulimallConnectApplication.class, args);
+    }
+
+    @Bean
+    public AppRunner appRunner() {
+        return new AppRunner();
+    }
+    class AppRunner implements ApplicationRunner {
+
+        @Autowired(required = false)
+        private OSS ossClient;
+
+        @Override
+        public void run(ApplicationArguments args) throws Exception {
+            try {
+                if (!ossClient.doesBucketExist(BUCKET_NAME)) {
+                    ossClient.createBucket(BUCKET_NAME);
+                }
+            } catch (Exception e) {
+                System.err.println("oss handle bucket error: " + e.getMessage());
+                System.exit(-1);
+            }
+        }
     }
 }
--- gulimall-connect/src/main/java/com/glm/connect/controller/OssController.java
+++ gulimall-connect/src/main/java/com/glm/connect/controller/OssController.java
@@ -1,11 +1,56 @@
 package com.glm.connect.controller;
 
+import com.aliyun.oss.OSS;
+import com.aliyun.oss.common.utils.IOUtils;
+import com.aliyun.oss.model.OSSObject;
+import com.glm.connect.GulimallConnectApplication;
+import org.apache.commons.codec.CharEncoding;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.core.io.Resource;
+import org.springframework.util.StreamUtils;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.nio.charset.Charset;
+
 @RestController
-@ConfigurationProperties("ossconfig")
 public class OssController {
 
+    @Autowired(required = false)
+    private OSS ossClient;
+    @Value("oss://" + GulimallConnectApplication.BUCKET_NAME + "/oss-test.json")
+    private Resource file;
+    @GetMapping("/upload")
+    public String upload() {
+        try {
+            ossClient.putObject(GulimallConnectApplication.BUCKET_NAME, "oss-test.json", this
+                    .getClass().getClassLoader().getResourceAsStream("oss-test.json"));
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "upload fail: " + e.getMessage();
+        }
+        return "upload success";
+    }
+    @GetMapping("/file-resource")
+    public String fileResource() {
+        try {
+            return "get file resource success. content: " + StreamUtils.copyToString(
+                    file.getInputStream(), Charset.forName(CharEncoding.UTF_8));
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "get resource fail: " + e.getMessage();
+        }
+    }
+    @GetMapping("/download")
+    public String download() {
+        try {
+            OSSObject ossObject = ossClient.getObject(GulimallConnectApplication.BUCKET_NAME, "oss-test.json");
+            return "download success, content: " + IOUtils
+                    .readStreamAsString(ossObject.getObjectContent(), CharEncoding.UTF_8);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "download fail: " + e.getMessage();
+        }
+    }
 }
