0bb06960bebade2d2f22c7fe81ba70950e4e8186
zehucai <caizh1997@live.com>
2022-11-21 12:57:28 +0800

    implement update for category in admin system

--- renren-fast-vue/src/views/modules/product/category.vue
+++ renren-fast-vue/src/views/modules/product/category.vue
@@ -19,6 +19,9 @@
           >
             Append
           </el-button>
+          <el-button type="text" size="mini" @click="() => update(data)">
+            Update
+          </el-button>
           <el-button
             v-if="node.childNodes.length == 0"
             type="text"
@@ -30,15 +33,24 @@
         </span>
       </span>
     </el-tree>
-    <el-dialog title="Confirm" :visible.sync="dialogVisible" width="30%">
+    <el-dialog :title="formTitle" :visible.sync="dialogVisible" width="30%">
       <el-form :model="formProps">
-        <el-form-item label="Category Name">
+        <el-form-item label="Name">
           <el-input v-model="formProps.name" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="Icon">
+          <el-input v-model="formProps.icon" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="Unit">
+          <el-input
+            v-model="formProps.productUnit"
+            autocomplete="off"
+          ></el-input>
         </el-form-item>
       </el-form>
       <span slot="footer" class="dialog-footer">
         <el-button @click="closedFormHandle">Cancel</el-button>
-        <el-button type="primary" @click="addCategoryHandle">Confirm</el-button>
+        <el-button type="primary" @click="formTypeHandle">Confirm</el-button>
       </span>
     </el-dialog>
   </div>
@@ -55,7 +67,18 @@ export default {
         children: "children",
         label: "name"
       },
-      formProps: { name: "", parentCid: 0, catLevel: 0, showStatus: 1, sort: 0 }
+      formTitle: "",
+      formType: "",
+      formProps: {
+        name: "",
+        parentCid: 0,
+        catLevel: null,
+        showStatus: 1,
+        sort: 0,
+        catId: null,
+        icon: "",
+        productUnit: ""
+      }
     };
   },
   methods: {
@@ -67,9 +90,16 @@ export default {
     },
     append(data) {
       console.log("append", data);
+      this.formType = "append";
+      this.formTitle = "Appending Category";
       this.dialogVisible = true;
       this.formProps.parentCid = data.catId;
       this.formProps.catLevel = data.catLevel * 1 + 1;
+      // reset properties so after update the value doesn't get carry over
+      this.formProps.catId = null;
+      this.formProps.name = "";
+      this.formProps.icon = "";
+      this.formProps.productUnit = "";
     },
     addCategoryHandle() {
       console.log("Appending Category", this.formProps);
@@ -80,6 +110,40 @@ export default {
       }).then(({ data }) => {
         this.$message({
           message: `${this.formProps.name} has been appended.`,
+          type: "success"
+        });
+        this.closedFormHandle();
+        this.getCategory();
+        this.expandedKey = [this.formProps.parentCid];
+      });
+    },
+    update(data) {
+      console.log(data);
+      this.formType = "update";
+      this.formTitle = "Updating Category";
+      this.dialogVisible = true;
+      // get the latest data to display on the form
+      this.$http({
+        url: this.$http.adornUrl(`/product/category/info/${data.catId}`),
+        method: "get"
+      }).then(({ data }) => {
+        this.formProps.name = data.data.name;
+        this.formProps.catId = data.data.catId;
+        this.formProps.icon = data.data.icon;
+        this.formProps.productUnit = data.data.productUnit;
+        this.formProps.parentCid = data.data.parentCid;
+      });
+    },
+    updateCategoryHandle() {
+      var { catId, name, icon, productUnit } = this.formProps;
+      var data = { catId, name, icon, productUnit };
+      this.$http({
+        url: this.$http.adornUrl("/product/category/update"),
+        method: "post",
+        data: this.$http.adornData(data, false)
+      }).then(({ data }) => {
+        this.$message({
+          message: `${this.formProps.name} has been updated.`,
           type: "success"
         });
         this.closedFormHandle();
@@ -120,6 +184,14 @@ export default {
     },
     closedFormHandle() {
       this.dialogVisible = false;
+    },
+    formTypeHandle() {
+      if (this.formType == "append") {
+        this.addCategoryHandle();
+      }
+      if (this.formType == "update") {
+        this.updateCategoryHandle();
+      }
     }
   },
   created() {
