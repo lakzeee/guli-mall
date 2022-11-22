175da5629026114949e0e741e370a469b0c345a7
zehucai <caizh1997@live.com>
2022-11-22 14:45:19 +0800

    create api for sending policy to alicloud oss

--- gulimall-connect/src/main/java/com/glm/connect/controller/OssController.java
+++ gulimall-connect/src/main/java/com/glm/connect/controller/OssController.java
@@ -1,19 +1,62 @@
 package com.glm.connect.controller;
 
 import com.aliyun.oss.OSS;
-import com.aliyun.oss.common.utils.IOUtils;
-import com.aliyun.oss.model.OSSObject;
-import com.glm.connect.GulimallConnectApplication;
-import org.apache.commons.codec.CharEncoding;
+import com.aliyun.oss.common.utils.BinaryUtil;
+import com.aliyun.oss.model.MatchMode;
+import com.aliyun.oss.model.PolicyConditions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.io.Resource;
-import org.springframework.util.StreamUtils;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.nio.charset.Charset;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 
 @RestController
 public class OssController {
+    @Autowired
+    OSS ossClient;
+
+    @Value("${spring.cloud.alicloud.oss.bucket}")
+    private String bucket;
+    @Value("${spring.cloud.alicloud.oss.endpoint}")
+    private String endpoint;
+    @Value("${spring.cloud.alicloud.access-key}")
+    private String accessId;
+
+    @RequestMapping("/oss/policy")
+    public Map<String, String> policy() {
+        String host = "https://" + bucket + "." + endpoint;
+        String simpleDate = new SimpleDateFormat("yyyy-MM-dd").format(new Date());
+        String dir = simpleDate + "/";
+        Map<String, String> respMap = null;
+        try {
+            long expireTime = 30;
+            long expireEndTime = System.currentTimeMillis() + expireTime * 1000;
+            Date expiration = new Date(expireEndTime);
+            PolicyConditions policyConds = new PolicyConditions();
+            policyConds.addConditionItem(PolicyConditions.COND_CONTENT_LENGTH_RANGE, 0, 1048576000);
+            policyConds.addConditionItem(MatchMode.StartWith, PolicyConditions.COND_KEY, dir);
+
+            String postPolicy = ossClient.generatePostPolicy(expiration, policyConds);
+            byte[] binaryData = postPolicy.getBytes("utf-8");
+            String encodedPolicy = BinaryUtil.toBase64String(binaryData);
+            String postSignature = ossClient.calculatePostSignature(postPolicy);
+
+            respMap = new LinkedHashMap<String, String>();
+            respMap.put("accessId", accessId);
+            respMap.put("policy", encodedPolicy);
+            respMap.put("signature", postSignature);
+            respMap.put("dir", dir);
+            respMap.put("host", host);
+            respMap.put("expire", String.valueOf(expireEndTime / 1000));
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        return respMap;
+    }
 }
