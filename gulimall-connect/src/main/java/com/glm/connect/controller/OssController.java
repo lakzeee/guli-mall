6550ea106ade26cd42a8a1dc5043111dbd47c182
zehucai <caizh1997@live.com>
2022-11-22 14:01:09 +0800

    test for oss upload success

--- gulimall-connect/src/main/java/com/glm/connect/controller/OssController.java
+++ gulimall-connect/src/main/java/com/glm/connect/controller/OssController.java
@@ -16,41 +16,4 @@ import java.nio.charset.Charset;
 
 @RestController
 public class OssController {
-
-    @Autowired(required = false)
-    private OSS ossClient;
-    @Value("oss://" + GulimallConnectApplication.BUCKET_NAME + "/oss-test.json")
-    private Resource file;
-    @GetMapping("/upload")
-    public String upload() {
-        try {
-            ossClient.putObject(GulimallConnectApplication.BUCKET_NAME, "oss-test.json", this
-                    .getClass().getClassLoader().getResourceAsStream("oss-test.json"));
-        } catch (Exception e) {
-            e.printStackTrace();
-            return "upload fail: " + e.getMessage();
-        }
-        return "upload success";
-    }
-    @GetMapping("/file-resource")
-    public String fileResource() {
-        try {
-            return "get file resource success. content: " + StreamUtils.copyToString(
-                    file.getInputStream(), Charset.forName(CharEncoding.UTF_8));
-        } catch (Exception e) {
-            e.printStackTrace();
-            return "get resource fail: " + e.getMessage();
-        }
-    }
-    @GetMapping("/download")
-    public String download() {
-        try {
-            OSSObject ossObject = ossClient.getObject(GulimallConnectApplication.BUCKET_NAME, "oss-test.json");
-            return "download success, content: " + IOUtils
-                    .readStreamAsString(ossObject.getObjectContent(), CharEncoding.UTF_8);
-        } catch (Exception e) {
-            e.printStackTrace();
-            return "download fail: " + e.getMessage();
-        }
-    }
 }
--- gulimall-connect/src/test/java/com/glm/connect/GulimallConnectApplicationTests.java
+++ gulimall-connect/src/test/java/com/glm/connect/GulimallConnectApplicationTests.java
@@ -1,13 +1,22 @@
 package com.glm.connect;
 
+import com.aliyun.oss.OSSClient;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import javax.annotation.Resource;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
 @SpringBootTest
 class GulimallConnectApplicationTests {
-
+    @Resource
+    OSSClient ossClient;
     @Test
-    void contextLoads() {
+    public void te() throws FileNotFoundException {
+        FileInputStream inputStream = new FileInputStream("/Users/lak/Downloads/oss-test-upload.png");
+        ossClient.putObject("gulimall-lak","oss-test-upload4.jpg",inputStream);
+        ossClient.shutdown();
+        System.out.println("上传完成");
     }
-
 }
