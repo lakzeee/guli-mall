1e834e735faafa46d95b472dec9599f5674191c3
zehucai <caizh1997@live.com>
2022-11-24 21:05:50 +0800

    implement save attr and attr-group-relation at the same time

--- gulimall-product/src/main/java/com/glm/product/service/AttrService.java
+++ gulimall-product/src/main/java/com/glm/product/service/AttrService.java
@@ -3,6 +3,7 @@ package com.glm.product.service;
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.glm.common.utils.PageUtils;
 import com.glm.product.entity.AttrEntity;
+import com.glm.product.vo.AttrVo;
 
 import java.util.Map;
 
@@ -16,5 +17,7 @@ import java.util.Map;
 public interface AttrService extends IService<AttrEntity> {
 
     PageUtils queryPage(Map<String, Object> params);
+
+    void saveAttrVo(AttrVo attr);
 }
 
--- gulimall-product/src/main/java/com/glm/product/service/impl/AttrServiceImpl.java
+++ gulimall-product/src/main/java/com/glm/product/service/impl/AttrServiceImpl.java
@@ -1,5 +1,10 @@
 package com.glm.product.service.impl;
 
+import com.glm.product.dao.AttrAttrgroupRelationDao;
+import com.glm.product.entity.AttrAttrgroupRelationEntity;
+import com.glm.product.vo.AttrVo;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.util.Map;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
@@ -11,10 +16,13 @@ import com.glm.common.utils.Query;
 import com.glm.product.dao.AttrDao;
 import com.glm.product.entity.AttrEntity;
 import com.glm.product.service.AttrService;
+import org.springframework.transaction.annotation.Transactional;
 
 
 @Service("attrService")
 public class AttrServiceImpl extends ServiceImpl<AttrDao, AttrEntity> implements AttrService {
+    @Autowired
+    AttrAttrgroupRelationDao relationDao;
 
     @Override
     public PageUtils queryPage(Map<String, Object> params) {
@@ -24,6 +32,18 @@ public class AttrServiceImpl extends ServiceImpl<AttrDao, AttrEntity> implements
         );
 
         return new PageUtils(page);
+    }
+
+    @Transactional
+    @Override
+    public void saveAttrVo(AttrVo attr) {
+        AttrEntity attrEntity = new AttrEntity();
+        BeanUtils.copyProperties(attr, attrEntity);
+        this.save(attrEntity);
+        AttrAttrgroupRelationEntity relationEntity = new AttrAttrgroupRelationEntity();
+        relationEntity.setAttrGroupId(attr.getAttrGroupId());
+        relationEntity.setAttrId(attrEntity.getAttrId());
+        relationDao.insert(relationEntity);
     }
 
 }
