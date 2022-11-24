872173dad491a641d590c115d80491fc1fa58dd4
zehucai <caizh1997@live.com>
2022-11-24 16:07:53 +0800

    implement search for brand mgmt

---
+++ gulimall-product/src/main/java/com/glm/product/config/MyBatisConfig.java
@@ -0,0 +1,21 @@
+package com.glm.product.config;
+
+import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@EnableTransactionManagement
+@MapperScan("com.glm.product.dao")
+public class MyBatisConfig {
+
+    @Bean
+    public PaginationInterceptor paginationInterceptor(){
+        PaginationInterceptor paginationInterceptor = new PaginationInterceptor();
+        paginationInterceptor.setOverflow(true);
+        paginationInterceptor.setLimit(1000);
+        return paginationInterceptor;
+    }
+}
